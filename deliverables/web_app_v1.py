5f8f83bbfc9128729138b88e3b997a57c27e2674
Aaron M. Carr <90634807+amcarr-ds@users.noreply.github.com>
2023-07-25 16:29:44 -0700

    Web app test

--- deliverables/web_app_v1.py
+++ deliverables/web_app_v1.py
@@ -2,14 +2,28 @@
 https://towardsdatascience.com/how-to-deploy-machine-learning-models-601f8c13ff45
 '''
 
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-file_in_name01 = 'data_preprocessed_wo_sw_X2_2023-07-25_13-11-04731013.csv'
+# Print current working directory (for debugging)
+print(os.getcwd())
 
-data = pd.read_csv(file_in_name01)
-display(data.head())
+# Print the list of files in the current directory (for debugging)
+print(os.listdir())
+
+# Try to read the file and handle any exceptions
+try:
+    data = pd.read_csv("data_preprocessed_wo_sw_X2_2023-07-25_13-11-04731013.csv")
+    st.write(data)
+except Exception as e:
+    st.error(f"Error: {e}")
+
+#file_in_name01 = 'data_preprocessed_wo_sw_X2_2023-07-25_13-11-04731013.csv'
+
+#data = pd.read_csv(file_in_name01)
+#display(data.head())
 
 st.header("Fish Weight Prediction App")
 st.text_input("Enter your Name: ", key="name")
