ae23c20525a618008951e25c6e14c32f3455c8a7
Aaron M. Carr <90634807+amcarr-ds@users.noreply.github.com>
2023-07-25 13:17:39 -0700

    Refine web app

--- deliverables/web_app_v1.py
+++ deliverables/web_app_v1.py
@@ -6,28 +6,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-'''Dir nav citation:
-https://softhints.com/python-change-directory-parent/
-'''
-curr_dir = os.path.abspath(os.curdir)
-print(curr_dir)
-os.chdir("..")
-up1_dir = os.path.abspath(os.curdir)
-print(up1_dir)
-
-# change `data_location` to the location of the folder on your machine.
-data_location = 'data'
-ref_docs_location = 'ref_docs'
-
 file_in_name01 = 'data_preprocessed_wo_sw_X2_2023-07-25_13-11-04731013.csv'
 
-file_in_path01 = os.path.join(up1_dir, data_location, file_in_name01)
-
-print(f'CSV file in 1 path: {file_in_path01}')
-
-data = pd.read_csv(file_in_path01)
+data = pd.read_csv(file_in_name01)
 display(data.head())
-#data = pd.read_csv("fish.csv")
 
 st.header("Fish Weight Prediction App")
 st.text_input("Enter your Name: ", key="name")
