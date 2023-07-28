a032b3927963c36d96aeaa05c6741743c51f0583
Aaron M. Carr <90634807+amcarr-ds@users.noreply.github.com>
2023-07-28 11:14:35 -0700

    Update ADS509_Team9_Capstone_07_Web_App_v1.py

--- deliverables/ADS509_Team9_Capstone_07_Web_App_v1.py
+++ deliverables/ADS509_Team9_Capstone_07_Web_App_v1.py
@@ -70,4 +70,6 @@ if st.button('Find articles rvsd'):
     data['multilabel'] = data['multilabel'].apply(ast.literal_eval)
     
     filtered_data = data[data['multilabel'].apply(lambda x: any(x[topic_dict[name]] == 1 for name in selected_topic_names))]
-    st.write(filtered_data)
+    fd_display_cols = ['publish_date', 'source_name', 'title', 'url']
+    filter_data_d = filtered_data[fd_display_cols]
+    st.write(filter_data_d)
