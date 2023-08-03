e262ce9930003228667b88c5e0c15b4a839b2878
Aaron M. Carr <90634807+amcarr-ds@users.noreply.github.com>
2023-08-03 11:11:32 -0700

    Update ref docs and web app

--- deliverables/ADS509_Team9_Capstone_07_Web_App.py
+++ deliverables/ADS509_Team9_Capstone_07_Web_App.py
@@ -13,10 +13,12 @@ import os
 import random
 import ast  # Import the ast module for literal_eval
 
+# Set random state; can be eliminated for production to create psuedo-random samples
 random_state = 1699
 random.seed(random_state)
 thresh = .9984
 
+# Set global sample return n
 article_n = 2
 article_nx2 = article_n * 3
 
@@ -28,49 +30,36 @@ os.listdir()
 
 # Try to read the file and handle any exceptions
 try:
-    #data01 = pd.read_csv('data_tm_wo_sw_Xy_half1_2023-07-28_10-51-08326790.csv')
-    #data02 = pd.read_csv('data_tm_wo_sw_Xy_half2_2023-07-28_10-51-08326790.csv')
-    #data = pd.concat([data01, data02], ignore_index=True)
     data = pd.read_csv('capstone_master_topic_assignment_0729.csv')
-    #y01_arr01 = np.load('data_tm_wo_sw_topic_lst_2023-07-28_10-51-08326790.npy')
-    #y01_arr01 = np.load('capstone_master_customer_topics.npy')
-    # Load pre-processed sentiment
-    #data_sa01 = pd.read_csv('data_sa_w_sw_half1_2023-07-28_12-11-33337653.csv')
-    #data_sa02 = pd.read_csv('data_sa_w_sw_half2_2023-07-28_12-11-33337653.csv')
-    #data_sa = pd.concat([data_sa01, data_sa02],
-    #                    ignore_index=True)
     data_sa = pd.read_csv('news-05.csv')
     data = pd.merge(data, data_sa, on='text_id')
     #st.write(data)
 except Exception as e:
     st.error(f'CWD: {os.getcwd()}\nList dir: {os.listdir()}\nError: {e}')
 
+# Set page header
 st.header('Your Daily Retreat: Positive News App')
 
+# Option to show raw dataframe
 if st.checkbox('Show dataframe'):
     data
 
+# Set col selection for aggregate df's
 group_by_cols = ['source_name', 'customer_topics', 'text_id',]
 
+# Compare summaries by different thresholds
 data1a = data.loc[data['sentiment_roberta'] > .8]
 data1a = data1a[group_by_cols]
 #st.write(len(data1a))
 #st.write(data1a.groupby(by=['source_name', 'customer_topics']).count())
-
 data1b = data.loc[data['sentiment_roberta'] > thresh]
 data1b = data1b[group_by_cols]
 #st.write(len(data1b))
 #st.write(data1b.groupby(by=['source_name', 'customer_topics']).count())
 
-#topic_lst = y01_arr01.tolist()
+st.subheader("Please select Topic(s) and/or Source(s)! Or just hit 'Find Articles' for a random selection")
 
-#topic_dict = {}
-#for idx, t in enumerate(topic_lst):
-#    topic_dict[t] = idx
-    
-#print(topic_dict)
-
-st.subheader('Please select Topic(s) and/or Source(s)!')
+# Display query filters
 left_column, right_column = st.columns(2)
 with left_column:
     selected_topics = st.multiselect(
@@ -82,18 +71,9 @@ with right_column:
         'Source Name:',
         data['source_name'].unique())
 
-if st.button('Find articles'):
-    #rev_topic_dict = {v: k for k, v in topic_dict.items()}
-    #selected_indices = [topic_dict[i] for i in selected_topics]
-    #selected_topic_names = [rev_topic_dict[idx] for idx in selected_indices]
-    #st.write(rev_topic_dict)
-    #st.write(selected_topic_names)
-    
-    # Convert the 'multilabel' column from string to list of integers
-    #data['multilabel'] = data['multilabel'].apply(ast.literal_eval)
-    
+# Run query to find sample articles based on user-specified filters
+if st.button('Find Articles'):
     filtered_data = data.loc[data['sentiment_roberta'] > thresh]
-    #filtered_data = filtered_data[filtered_data['multilabel'].apply(lambda x: any(x[topic_dict[name]] == 1 for name in selected_topic_names))]
     topic_len = len(selected_topics)
     #st.write(topic_len)
     source_len = len(selected_sources)
@@ -101,9 +81,11 @@ if st.button('Find articles'):
     #for t in selected_topics:
     #    st.write(t)
     
+    # Col selection for display & sort
     fd_display_cols = ['customer_topics', 'source_name', 'url', 'title', 'publish_date',]
     fd_sort = ['customer_topics', 'source_name', 'publish_date',]
-
+    
+    # Condition 1: No sources or topics selected
     if topic_len == 0 and source_len == 0:
         filtered_data_s1 = filtered_data.sample(article_nx2,
                                                 random_state=random_state)
@@ -114,8 +96,8 @@ if st.button('Find articles'):
         st.write(f'No search criteria entered: {len(filtered_data_s1)} randomly returned')
         #st.write(filtered_data01)
 
+    # Condition 2: No sources selected
     elif topic_len > 0 and source_len == 0:
-        #filtered_data = filtered_data.loc[filtered_data['customer_topics'].isin(selected_topics)]
         filtered_data_s1 = pd.DataFrame()
         for t in selected_topics:
             filtered_data_s2 = filtered_data.loc[filtered_data['customer_topics'] == t]
@@ -134,8 +116,8 @@ if st.button('Find articles'):
         st.write(f"Sample for '{selected_topics}': {len(filtered_data_s1)} randomly returned")
         #st.write(filtered_data02)
 
+    # Condition 3: No topics selected
     elif topic_len == 0 and source_len > 0:
-        #filtered_data = filtered_data.loc[filtered_data['source_name'].isin(selected_sources)]
         filtered_data_s1 = pd.DataFrame()
         for s in selected_sources:
             filtered_data_s2 = filtered_data.loc[filtered_data['source_name'] == s]
@@ -154,9 +136,8 @@ if st.button('Find articles'):
         st.write(f"Sample for '{selected_sources}': {len(filtered_data_s1)} randomly returned")
         #st.write(filtered_data03)
 
+    # Condition 4: Sources and topics selected
     else:
-        #filtered_data = filtered_data.loc[filtered_data['source_name'].isin(selected_sources)]
-        #filtered_data = filtered_data.loc[filtered_data['customer_topics'].isin(selected_topics)]
         filtered_data_s1 = pd.DataFrame()
         for s in selected_sources:
             for t in selected_topics:
@@ -175,19 +156,21 @@ if st.button('Find articles'):
                                                         ascending=False)
         st.write(f"Sample for '{selected_sources}' and '{selected_topics}': {len(filtered_data_s1)} randomly returned")
         #st.write(filtered_data04)
+        
+    # Use streamlit method to format resulting dataframe
     st.data_editor(
         filtered_data_s1,
         column_config={
             'url': st.column_config.LinkColumn('Article Link',
                                                width='large'),
             'customer_topics': st.column_config.TextColumn('Topic',
-                                                           width='small'),
+                                                           width='medium'),
             'source_name': st.column_config.TextColumn('Source',
-                                                       width='small'),
+                                                       width='medium'),
             'title': st.column_config.TextColumn('Article Title',
                                                  width='large'),
             'publish_date': st.column_config.TextColumn('Publish Date',
-                                                        width='small'),
+                                                        width='medium'),
         },
         hide_index=True,)
         
